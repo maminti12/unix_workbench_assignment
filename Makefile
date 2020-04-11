05f9cf4beeebe6ce0b8496653a50861f53fcd5ac
maminti12 <maminti12@gmail.com>
2020-04-11 20:32:30 +0200

    files created

---
+++ Makefile
@@ -0,0 +1,6 @@
+readme: create 
+	echo "# Guessing game - Submisson for assignment" >> README.md
+	date >> README.md
+	wc -l guessinggame.sh >> README.md
+create:
+	touch README.md
---
+++ guessinggame.sh
@@ -0,0 +1,30 @@
+function request {
+	echo "How many files are in the current directory? Guess the number, please!"
+read resp
+let n=$resp
+}
+request
+
+rn=$(ls -A | wc -l)                     # real number of files including dot files
+
+while [[ n -ne rn ]] 
+do
+	if ! [[ $resp =~ ^[0-9]+$ ]]
+	then
+		echo "Please enter a valid number!!!"
+	elif [[ n -lt rn ]]
+	then 
+		echo "It's too low, try a greather number"
+	elif [[ n -gt rn ]]
+	then 
+		echo "It's too big, try a smaller number"
+	else
+		echo ""
+	fi 
+	request	
+done 
+
+if [[ n -eq rn ]] 
+then 
+	echo "That's it, $rn. Congratulations!"
+fi
